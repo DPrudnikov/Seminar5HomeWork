e3249b8bc2e9b9709da6e0c9683ba9779fbcf9c4
DPrudnikov <dima1911@mail.ru>
2023-06-05 22:43:04 +0300

    add ex34

---
+++ Ex34/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int[] array = new int [7];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(99,1000);
+    Console.Write(array[i] + " "); 
+
+}
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i] % 2;
+    Console.Write(array[i]+ " ");
+    if (array[i] == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine(count);
