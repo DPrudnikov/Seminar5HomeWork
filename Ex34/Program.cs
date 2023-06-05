6f53578b68bb522fcb7962b82b0fb1b18958cfbe
DPrudnikov <dima1911@mail.ru>
2023-06-05 23:39:31 +0300

    add new ex36

--- Ex34/Program.cs
+++ Ex34/Program.cs
@@ -5,7 +5,7 @@ int count = 0;
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(99,1000);
-    Console.Write(array[i] + " "); 
+    Console.Write(array[i] + ", "); 
 
 }
 Console.WriteLine();
---
+++ Ex36/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int [] array = new int [7];
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(10,100);
+    Console.Write(array[i] + ", ");
+}
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 == 1)
+    {
+        sum = sum + array[i];
+    }
+}
+Console.WriteLine(sum);
